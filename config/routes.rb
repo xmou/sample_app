21b94ec358352bca493979ee7edb3cd6f4eeb551
xmou <xmou@thoughtworks.com>
2012-10-28 22:28:15 +0800

    Added sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.routes.draw do
   root to: 'static_pages#home'
 
+  resources :users
+
   match '/signup', to: 'users#new'
   match '/help', to: 'static_pages#help' 
   match '/about', to: 'static_pages#about' 
